6e83d723354365cb65bd6fdabfec2fe7983f4196
Cesar Augusto <augustocsar22@gmail.com>
2023-11-26 05:32:41 -0300

    tela de autenticação

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+import { AutenticacaoProvider } from "./src/context/AutenticacaoContext";
 import { TemaProvider } from "./src/context/TemaContext";
 import Rotas from "./src/rotas";
 
 export default function App() {
   return (
     <TemaProvider>
-      <Rotas />
+      <AutenticacaoProvider>
+        <Rotas />
+      </AutenticacaoProvider>
     </TemaProvider>
   );
 }
